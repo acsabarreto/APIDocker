a7e3e0216fab745b68626e300812081c54612567
Antônio Sá Barreto <acsabarreto@gmail.com>
2022-02-11 17:54:41 -0300

    remove gunicorn timeout

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #pip install -r requirements.txt
-gunicorn --bind 0.0.0.0:27017 --workers "${WORKERS}" app:app
+gunicorn --bind 0.0.0.0:27017 --workers "${WORKERS}" --timeout ${TIMEOUT} app:app
